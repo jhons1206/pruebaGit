2b72f8a22f49ddaa153d9dacfee6a7012e7c8b6b
jhons1206 <webdeveloper84@gmail.com>
2024-10-03 22:25:43 -0500

    Mi primer programa en C++

---
+++ Programa1.cpp
@@ -0,0 +1,11 @@
+def realizar_operaciones_basicas():
+    a = 15
+    b = 10
+    suma = a + b
+    resta = a - b
+    producto = a * b
+    division = a / b
+    print(f"Operaciones con {a} y {b} - Suma: {suma}, Resta: {resta}, Producto: {producto}, División: {division:.2f}")
+
+realizar_operaciones_basicas()
+
